64ad494c88496f686d9a02a13ad0356c60923a2b
akshaybankar007 <akshaybankar076@gmail.com>
2026-05-24 14:50:28 +0530

    Remove old hardcoded product cards from HTML and bring them dynamically

--- products.js
+++ products.js
@@ -5,16 +5,40 @@ const products = [
     {
         brand: "Roadster",
         name: "Men Casual Shirt",
-        price: 999
+        price: 999,
+        image: "Assets/men/men-1.jpg"
     },
     {
         brand: "HRX",
         name: "Everyday Outfit",
-        price: 1499
+        price: 1499,
+        image: "Assets/men/men-2.jpg"
     },
     {
         brand: "Levi's",
         name: "Denim Street Style",
-        price: 1499
+        price: 1399,
+        image: "Assets/men/men-3.jpg"
     }
 ];
+
+products.forEach((product) => {
+    console.log(product)
+});
+
+products.forEach((product) => {
+    productContainer.innerHTML += `
+    <article class="product-card">
+     <div class="card-image-wrap">
+        <img src="${product.image}">
+        </div>
+
+        <div class=product-details>
+        <h3 class="product-brand">${product.brand}</h3>
+        <p class="product-name">${product.name}</p>
+        <p class="product-price">₹${product.price}</p>
+        </div>
+    </article>
+    `;   
+     });
+     
