1310eb7297aed92f7554a04cb1bcc71ae7206a2d
akshaybankar007 <akshaybankar076@gmail.com>
2026-05-24 13:55:49 +0530

    Add product container ID to index.html and initialize products in products.js

---
+++ products.js
@@ -0,0 +1,20 @@
+const productContainer = document.getElementById('productContainer');
+console.log(productContainer);
+
+const products = [
+    {
+        brand: "Roadster",
+        name: "Men Casual Shirt",
+        price: 999
+    },
+    {
+        brand: "HRX",
+        name: "Everyday Outfit",
+        price: 1499
+    },
+    {
+        brand: "Levi's",
+        name: "Denim Street Style",
+        price: 1499
+    }
+];
